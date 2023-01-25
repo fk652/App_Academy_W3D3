9f2c64799a9ce00ad5332971de75d4c85724bd13
kevalshah598 <kevalshah598@gmail.com>
2023-01-25 13:52:07 -0500

    exponentiation

--- recursion.rb
+++ recursion.rb
@@ -14,3 +14,28 @@ def range_iter(num_1, num_2)
   final_arr
 end
 p range_iter(1, 5)
+
+def exp(base, power)
+  return 0 if base == 0
+  return 1 if power == 0
+  base * exp(base, power - 1)
+end
+
+p exp(0, 5)
+p exp(3, 3)
+p exp(4, 2)
+
+def exp_2(base, power)
+  return 1 if power == 0
+  return base if power == 1
+  if power.even?
+    exp_2(base, power/2) ** 2
+  elsif power.odd?
+    base * (exp_2(base, (power - 1)/ 2) ** 2)
+  end
+end
+
+p exp_2(2, 2)
+p exp_2(2, 3)
+p exp_2(2, 10)
+p exp_2(2, 11)
